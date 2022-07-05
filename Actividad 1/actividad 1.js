1452fec47d952f39a854aa018d3dbde1e9d8f1c0
JOEL FRANCISCO CASTAÑEDA DOMINGUEZ <joel.castaneda@coppel.com>
2022-07-05 17:10:48 -0600

    primer actividad

---
+++ Actividad 1/actividad 1.js
@@ -0,0 +1,33 @@
+
+//Ejercicio 1
+const multiplo=2;
+
+var num=prompt("escriba un numero")
+
+console.log("el resultado es: ",num*multiplo)
+
+//Ejercicio 2
+var numero1=prompt("escriba el primer numero")
+
+var numero2=prompt("escriba el segundo numero")
+
+var total=numero1-numero2
+
+console.log("el resultado es ", total)
+
+//Ejercicio 3
+
+const pi=3.1416
+
+var radio=prompt("escriba el radio")
+
+var area=(pi*(radio**2))
+
+console.log("el area del circulo es ", area)
+
+//Ejercicio 4
+var D=8, d=6
+
+var area=(D*d)/2
+
+console.log("el area es ",area)
